73be2ae87716781303d131e7e02276b2f757ad03
KuriosKat <101084482+KuriosKat@users.noreply.github.com>
2023-08-19 02:02:34 +0900

    Update config.py

--- config.py
+++ config.py
@@ -16,26 +16,6 @@ file = ""
 f = ''
 fwriter = ''
 
-Voicecontrol = False
-
-AIcontrol = False
-modelheight = -160 ###-130 ###-150 #-115 #-130 #-150 #-250 #-200
-
-# training speed setting
-maxturn_speed = 60
-minturn_speed = 5  ###20  ###15
-normal_speed_left = 20
-normal_speed_right = 20
-wheel_alignment_left = 0
-wheel_alignment_right = 0
-
-
-# testing speed setting(
-ai_maxturn_speed = 100
-ai_minturn_speed = 20
-ai_normal_speed_left = 100
-ai_normal_speed_right = 100
-
 
 
 
